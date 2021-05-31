25cad193192669e4af579f858e0e9bdb061050bf
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2021-05-31 19:21:31 -0400

    looping over the component

--- client/app1/src/data-file-table/index.jsx
+++ client/app1/src/data-file-table/index.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { getFileNames } from '../services/index.js';
-import Button from 'react-bootstrap/Button';
 
 const DataFileTable = (props) => {
 
   const[loadedFileNames, setLoadedFileNames] = useState(false);
-  const[files, setFiles] = useState(null);
+  const[files, setFiles] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,17 +15,38 @@ const DataFileTable = (props) => {
     fetchData();
   }, []);
 
+  const filesTable = files.map((file, index) => {
+     return <div className="" key={index}>
+       { index === 0 ?
+         (
+           <div className="row border" key={index}>
+             <p className="col-1 col-lg-1 mt-3">Index:</p>
+             <p className="col-4 col-lg-2 my-auto">Filename:</p>
+             <p className="col-2 col-lg-2 my-auto">Download:</p>
+           </div>
+         )
+         : null 
+       }
+
+       <div className="row border" key={index}>
+         <p className="col-1 col-lg-1 mt-3">{ index + 1 }</p>
+         <p className="col-4 col-lg-2 mt-3">{ file }</p>
+         <input type="button"className="btn btn-primary col-2 col-lg-2 my-auto" value="DOWNLOAD"></input>
+       </div>
+     </div>
+  });
   
   return (
     <div>
-      <h2>Here</h2>
-      <input type="button"className="btn btn-primary" value="DOWNLOAD"></input>
+      <h2>Downloadable Test Data:</h2>
+
+      <div className="container">
+        {filesTable}
+      </div>
+
+      
       
     </div>
-    
-
-
-
   );
 
 }
