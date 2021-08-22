cb0f70e4d5a9b56bac23037410d9615ae3e4bf4b
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2021-08-22 19:54:35 -0400

    changed route names for consistency with other platforms requesting our API

--- routes/mongo-routes.js
+++ routes/mongo-routes.js
@@ -16,7 +16,7 @@ router.get('/file-names', async (req, res) => {
   });
 });
 
-router.get('/download-file', async (req, res) => {
+router.get('/get-file', async (req, res) => {
   const { fileName } = req.query;
   const file = await mongoConnection.getFile(fileName);
 
