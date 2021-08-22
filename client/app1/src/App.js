9f093994d9eb2095b67c65aa298f34239e786ea0
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2021-08-21 20:28:25 -0400

    Add a new route for uploading

--- client/app1/src/App.js
+++ client/app1/src/App.js
@@ -1,10 +1,25 @@
 import DataFileTable from './data-file-table/index.jsx'
 import './App.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 export default function App() {
   return (
     <div>
-      <DataFileTable />
+      <BrowserRouter>
+        <Switch>
+          
+          <Route path="/upload-file">
+            <h2>Select a File to Upload</h2>
+          </Route>
+
+          <Route path="/">
+            <DataFileTable />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+      
     </div>
   );
 }
